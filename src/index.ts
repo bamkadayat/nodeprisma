bdb826673dbbfa879b545d211c2a3f6608e681ba
bamkadayat <bamkadayat@gmail.com>
2023-11-06 12:53:59 +0100

    update installation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3000; // You can choose any port
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello, world!");
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
+
+export {}; // Add this if you don't have any other exports
