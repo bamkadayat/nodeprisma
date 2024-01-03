162e28facdfb2afe6ef6bf0cfeddaf903ae9580a
bamkadayat <bamkadayat@gmail.com>
2024-01-03 16:31:13 +0100

    added json field to model Product

---
+++ prisma/migrations/20240103153006_init/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `Product` table. All the data in the column will be lost.
+  - Added the required column `title` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "description",
+DROP COLUMN "name",
+ADD COLUMN     "additionalInfo" JSONB,
+ADD COLUMN     "title" TEXT NOT NULL;
